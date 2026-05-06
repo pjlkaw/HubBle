7843a1f75cf9ebac3f7389541e2413c9ab4710e7
pjlkaw <pedrorochabispo@gmail.com>
2026-05-06 20:28:12 -0300

    Servidor e Index no servidor

---
+++ servidor/server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const path = require("path")
+
+const app = express()
+
+//Permite acessar CSS, JS, Imagens, ícones
+app.use(express.static(path.join(__dirname, "..public")))
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/index.html"))
+})
+
+app.listen(3000, (res, req) => {
+    console.log("Rodando na porta 3000");
+})

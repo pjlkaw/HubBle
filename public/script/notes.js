32617fd2ac15e3cbad2768a53f76c9aa1f0864fe
pjlkaw <pedrorochabispo@gmail.com>
2026-05-14 20:46:46 -0300

    funciona a adição de notas, mas otimizar

--- public/script/notes.js
+++ public/script/notes.js
@@ -1,7 +1,43 @@
 import { themeFunction } from '../functions/themeFunction.js'
 themeFunction(false, true) 
 
+let noteID = []
 
+const workspaceSection = document.getElementById('workspace')
 function workspace() {
+    addNote()
+    
 
 } workspace()
+
+function addNote() {
+    let noteIndex = 0
+    document.getElementById('addNote').addEventListener('click', ()=>{
+        noteIndex++
+
+        // cria nota
+        const createNote = document.createElement('div');
+        createNote.classList.add('note');
+        
+        createNote.innerHTML = `            
+            <div class="noteHead">
+                <p id="noteTittle${noteIndex}">Nota ${noteIndex}</p>
+            </div>
+            <div id="note${noteIndex}" class="noteBody">
+                <p id="noteContent${noteIndex}">Conteúdo da nota 1a</p>
+            </div>
+        `;
+        workspaceSection.appendChild(createNote) 
+        
+        // envia o ID da nota
+        noteID.push('note'+noteIndex)
+        console.log(noteID);
+        
+        // ao clique da nota criada
+        createNote.addEventListener('click', () => {
+            document.getElementById('modal').style.display = "flex"
+            
+
+        })
+    })
+}
